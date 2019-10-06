8ceb6d788e970c129da7ef3c454a59e05782ece0
scintilla4evr <kacper17@outlook.com>
2019-10-06 19:19:19 +0200

    Updated the service worker

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const DEV_BUILD_VERSION = 20
+const DEV_BUILD_VERSION = 21
 
 const CACHE_NAME = `static-cache-g4.6-stable${DEV_BUILD_VERSION}`;
 const DATA_CACHE_NAME = `static-cache-g4.6-stable${DEV_BUILD_VERSION}`;
