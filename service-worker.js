d335d5f436f6b340ed59119231dbbb641e0a1fe2
scintilla4evr <kacper17@outlook.com>
2019-10-22 16:29:26 +0200

    iOS layout fix

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // bush me uwu
-const DEV_BUILD_VERSION = 23
+const DEV_BUILD_VERSION = 30
 
 const CACHE_NAME = `static-cache-g4.6-stable${DEV_BUILD_VERSION}`;
 const DATA_CACHE_NAME = `static-cache-g4.6-stable${DEV_BUILD_VERSION}`;
